0cf75ecc8155265d0460918102df6ca7c84df2b6
PRI2206 <pr63547890@gecg28.ac.in>
2024-05-25 09:23:35 +0530

    starting using mui

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Footer.css';
+import linkedinIcon from '../../assets/photos/icons/linkedinIcon.png'
+import gitHubIcon from '../../assets/photos/icons/gitHubIcon.png'
 
-export default function Footer({themeIndex}) {
+export default function Footer({ themeIndex }) {
     const footerTheme = [{
         bgcolor: "bg-dark",
-        textColor:"text-white",
-        muteTextColor:"text-white-50",
+        textColor: "text-white",
+        muteTextColor: "text-white-50",
     },
     {
         bgcolor: "bg-body-secondary",
         textColor: "",
-        muteTextColor:"text-muted"
+        muteTextColor: "text-muted"
 
     }
     ]
@@ -19,16 +21,17 @@ export default function Footer({themeIndex}) {
             <div className={` ${footerTheme[themeIndex].bgcolor} `}>
                 <footer className="d-flex flex-wrap justify-content-between align-items-center py-3">
                     <div className="col-md-4 d-flex align-items-center">
-                        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                            <svg className="bi" width="30" height="24"></svg>
-                        </a>
+                        <svg className="bi" width="30" height="24"></svg>
                         <span className={`mb-3 mb-md-0  ${footerTheme[themeIndex].muteTextColor}`}>© 2023 Prince Patel, All rights reserved <br /> Icon providei by <a href="https://www.flaticon.com/">Flaticon</a> </span>
                     </div>
 
                     <ul className="nav col-md-4 justify-content-end align-items-center list-unstyled d-flex">
-                        <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/prince-patel-7900b8212"><img className="link-icon" src="photos/icons/linkedinIcon.png" alt="linkedin" /></a></li>
-                        <li className="ms-3"><a className="text-muted" href="https://github.com/PRIPATEL2206?tab=repositories"><img className="link-icon" src="photos/icons/gitHubIcon.png" alt="GitHub" /></a></li>
+                        <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/prince-patel-7900b8212"><img className="link-icon" src={linkedinIcon} alt="linkedin" /></a></li>
+                        <li className="ms-3"><a className="text-muted" href="https://github.com/PRIPATEL2206?tab=repositories"><img className="link-icon" src={gitHubIcon} alt="GitHub" /></a></li>
+                        <svg className="bi" width="30" height="24"></svg>
+
                     </ul>
+
                 </footer>
             </div>
         </div>
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,5 +1,57 @@
 import { Link } from 'react-router-dom';
 import "./NavBar.css";
+import profileImage from "../../assets/photos/profile.png"
+import { Button, FormControlLabel, Switch, styled } from '@mui/material';
+
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+    width: 62,
+    height: 34,
+    padding: 7,
+    '& .MuiSwitch-switchBase': {
+        margin: 1,
+        padding: 0,
+        transform: 'translateX(6px)',
+        '&.Mui-checked': {
+            color: '#fff',
+            transform: 'translateX(22px)',
+            '& .MuiSwitch-thumb:before': {
+                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                    '#fff',
+                )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+            },
+            '& + .MuiSwitch-track': {
+                opacity: 1,
+                backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+            },
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+        width: 32,
+        height: 32,
+        '&::before': {
+            content: "''",
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            left: 0,
+            top: 0,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+                '#fff',
+            )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+        },
+    },
+    '& .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        borderRadius: 20 / 2,
+    },
+}));
+
+
 
 export default function NavBar({ themeIndex, changeThemeIndex }) {
     function expendCollaps() {
@@ -19,40 +71,60 @@ export default function NavBar({ themeIndex, changeThemeIndex }) {
             <nav className={`navbar navbar-expand-sm bg-${navTheme[themeIndex]}`} data-bs-theme={`${navTheme[themeIndex]}`}>
                 <div className="container-fluid" >
                     <Link to="/" onClick={expendCollaps} className='navbar-brand'>
-                        <img src="photos/profile.png" className="navIcon" alt="profile" srcSet="" />
+                        <img src={profileImage} className="navIcon" alt="profile" srcSet="" />
                     </Link>
                     <h4>Prince Patel</h4>
-                    <button id='collapsButton' className={`navbar-toggler bg-${themeIndex!==0?'':'light'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation" style={{ "boxShadow": "none" }}>
+                    <button id='collapsButton' className={`navbar-toggler bg-${themeIndex !== 0 ? '' : 'light'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation" style={{ "boxShadow": "none" }}>
                         <span className="navbar-toggler-icon" ></span>
                     </button>
                     <div className="collapse navbar-collapse  " style={{ overflow: "hidden" }} id="navbarTogglerDemo02">
-                        <ul className="navbar-nav me-3 ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item ">
-                                <Link to="/" onClick={expendCollaps} className="nav-link" aria-current="page" > Home </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/About" onClick={expendCollaps} className="nav-link" > About </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/Skill&Tools" onClick={expendCollaps} className="nav-link" > Skill & Tools </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/Projects" onClick={expendCollaps} className="nav-link" > Projects </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/Services" onClick={expendCollaps} className="nav-link" > Services </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/ContectMe" onClick={expendCollaps} className="nav-link" > Contact Me</Link>
-                            </li>
-                            <li className="nav-item">
-                                <a  href='pdfs/Resume.pdf' download type='button' className='btn btn-outline-info me-4 ms-2'>Resume</a>
-                            </li>
-                            <li className="nav-item swith-item">
-                                <div className="form-check form-switch ">
-                                    <input className="form-check-input" type="checkbox" onClick={toggleTheme} role="switch" id="flexSwitchCheckChecked"  defaultChecked />
-                                </div>
-                            </li>
+                        <ul className="navbar-nav me-3 ms-auto mb-2 mb-lg-0 d-flex align-items-center">
+                            <Link to="/" onClick={expendCollaps} className="nav-link" aria-current="page" >
+                                <Button variant="text" style={{ color: themeIndex === 0 ? "white" : "black" }} >
+                                    Home
+                                </Button>
+                            </Link>
+                            <Link to="/About" onClick={expendCollaps} className="nav-link" >
+                                <Button variant="text" >
+                                    About
+                                </Button>
+                            </Link>
+
+                            <Link to="/Skill&Tools" onClick={expendCollaps} className="nav-link" >
+                                <Button variant="text">
+                                    Skill & Tools
+                                </Button>
+                            </Link>
+
+                            <Link to="/Projects" onClick={expendCollaps} className="nav-link" >
+                                <Button variant="text">
+                                    Projects
+                                </Button>
+                            </Link>
+
+                            <Link to="/Services" onClick={expendCollaps} className="nav-link" >
+                                <Button variant="text">
+                                    Services
+                                </Button>
+                            </Link>
+
+                            <Link to="/ContectMe" onClick={expendCollaps} className="nav-link" >
+                                <Button variant="text">
+                                    Contact Me
+                                </Button>
+                            </Link>
+
+
+                            <FormControlLabel
+                                control={<MaterialUISwitch sx={{ m: 1, mr: 0 }} defaultChecked onChange={toggleTheme} />}
+                            />
+
+                            <a href='pdfs/Resume.pdf' className='' download >
+                                <Button variant="outlined">
+                                    Resume
+                                </Button>
+                            </a>
+
                         </ul>
 
                     </div>
